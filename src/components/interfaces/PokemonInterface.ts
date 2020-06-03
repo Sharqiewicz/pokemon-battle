3ed2508534d0fd7d08583d72e9bb581dc7e43088
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-03 18:07:23 +0200

    add Pokemon interface

--- src/components/interfaces/PokemonInterface.ts
+++ src/components/interfaces/PokemonInterface.ts
@@ -1,5 +1,44 @@
 export default interface PokemonInterface{
     name: string;
+    height: number;
+    base_experience: number;
     atack: number;
-    imageUrl: string;
+    weight: number;
+    abilities: Abilities[];
+    sprites: Sprites[];
+    stats: Stats[];
+    types: Type[]
+}
+
+type Sprites = {
+    back_default: string,
+    back_female: string,
+    back_shiny: string,
+    back_shiny_female: string,
+    front_default: string,
+    front_female: string,
+    front_shiny: string,
+    front_shiny_female: string,
+}
+
+type Ability = {
+    name: string
+}
+
+type Abilities = {
+    ability: Ability
+}
+
+type Stat = {
+    name: string
+}
+
+type Stats = {
+    base_stat: number,
+    effort: number,
+    stat: Stat
+}
+
+type Type = {
+    name: string
 }
