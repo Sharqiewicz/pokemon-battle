b396ae0b3dcdb8422ac7e93d88de71ed01975614
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-06-03 17:39:34 +0200

    implement working loading pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,51 @@
 import React, { FunctionComponent } from 'react';
 import Loading from './components/loading/Loading'
+import api from './components/api/api-pokemon'
 
-function App()  {
-  return (
-    <div className="App">
-      <h2>pokemon battle</h2>
-      <Loading/>
-    </div>
-  );
+
+
+
+type PokeProps = {}
+
+type PokeState = {
+  pokemon: Pokemon
+}
+
+type Pokemon = {
+  name: string
+}
+
+
+class App extends React.Component <PokeProps, PokeState>  {
+  constructor(props: any){
+      super(props);
+      this.state = {
+        pokemon: {name: 'loading'}
+      }
+      this.getPokemon = this.getPokemon.bind(this);
+
+  }
+
+  getPokemon = async (name: string) =>{
+    const pokemon: Pokemon = await api.get('pikachu');
+    this.setState( {
+      pokemon: pokemon
+    }
+  , () => {  console.log(this.state.pokemon) })
+    return pokemon;
+  }
+
+  componentDidMount(){
+    this.getPokemon('pikachu');
+  }
+  render(){
+    return (
+      <div className="App">
+        <h2>pokemon battle</h2>
+        <h2>{this.state.pokemon ? this.state.pokemon.name: 'noah'}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
